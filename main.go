d850cb6e21da883e1523c9443557d22ce4d52eca
Maksim Cherkasov <cherkasov.maksim@globinform.ru>
2018-07-05 15:30:43 +0300

    udp server/client

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"crypto/ecdsa"
+	"ethereum_from_scratch/rlp"
 	"fmt"
 	"net"
+
+	"time"
+
+	"github.com/ethereum/go-ethereum/crypto/secp256k1"
 )
 
 type Node struct {
@@ -26,7 +33,10 @@ func (node *Node) Start() (<-chan string, <-chan string) {
 
 func (node *Node) listen(receive chan string) {
 	udpAddr, _ := net.ResolveUDPAddr("udp", node.endPoint.UdpAddress())
-	conn, _ := net.ListenUDP("udp", udpAddr)
+	conn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		log.Error(err.Error())
+	}
 	defer conn.Close()
 
 	buffer := make([]byte, 4096)
@@ -38,38 +48,77 @@ func (node *Node) listen(receive chan string) {
 	}
 }
 
-func (node *Node) Send(payload chan string, endpoint EndPoint) {
+//func (node *Node) wrapPacket(to EndPoint) {
+//
+//hash || signature || packet-type || packet-data
+//hash: sha3(signature || packet-type || packet-data)
+//signature: sign(privkey, sha3(packet-type || packet-data))
+//packet-type: single byte < 2**7 // valid values are [1,4]
+//packet-data: RLP encoded list. Packet properties are serialized in the order in which they're defined. See packet-data below.
+//}
+
+func (node *Node) SendPing(to EndPoint, key ecdsa.PrivateKey) {
+	ping := PingNode{
+		EndpointFrom: node.endPoint,
+		EndpointTo:   to,
+	}
+
+	var b bytes.Buffer
+	rlp.Encode(&b, ping.pack())
+	payload := bytes.Join([][]byte{{ping.packetType}, b.Bytes()}, []byte{})
+
+	sha3Payload := Keccak256(payload)
+	signature, _ := secp256k1.Sign(sha3Payload, key.D.Bytes())
+
+	payload = bytes.Join([][]byte{signature, payload}, []byte{})
+	payloadHash := Keccak256(payload)
+
+	result := bytes.Join([][]byte{payloadHash, payload}, []byte{})
+	node.Send(result, to)
+}
+
+func (node *Node) Send(payload []byte, to EndPoint) {
 	localAddr, _ := net.ResolveUDPAddr("udp", node.endPoint.UdpAddress())
-	destinationAddress, _ := net.ResolveUDPAddr("udp", endpoint.UdpAddress())
-	connection, _ := net.DialUDP("udp", localAddr, destinationAddress)
+	destinationAddress, _ := net.ResolveUDPAddr("udp", to.UdpAddress())
+	connection, err := net.DialUDP("udp", localAddr, destinationAddress)
+	if err != nil {
+		log.Error(err.Error())
+	}
 	defer connection.Close()
 
-	for {
-		message := <-payload
-		connection.Write([]byte(message))
-	}
+	//for {
+	connection.Write(payload)
+	//}
 }
 
 func main() {
 	ep1 := EndPoint{
-		Ip:      "127.0.0.1",
+		Ip:      "192.168.0.198",
 		UdpPort: 30303,
 		TcpPort: 30303,
 	}
 	//
-	//ep2 := EndPoint{
-	//	Ip:"13.93.211.84",
-	//	UdpPort: 30303,
-	//	TcpPort: 30303,
-	//213.208.177.250}
-	//wallet := InitWallet()
+	ep2 := EndPoint{
+		Ip:      "127.0.0.1",
+		UdpPort: 30303,
+		TcpPort: 30303,
+	}
+	wallet := InitWallet()
 	newNode := NewServer(ep1)
 	receive, _ := newNode.Start()
 
-	for {
-		select {
-		case msg := <-receive:
-			fmt.Printf("received message %s", msg)
+	go func() {
+		for {
+			select {
+			case msg := <-receive:
+				fmt.Printf("received message %s", msg)
+			}
 		}
-	}
+	}()
+
+	time.Sleep(2 * time.Second)
+
+	newNode.SendPing(ep2, wallet.PrivateKey)
+
+	select {}
 }
